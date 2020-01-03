cfc18d2f177ffc42182d3704ee7b552b4c9fa16c
farhan048 <achmadfarhanmushafa@gmail.com>
2020-01-03 16:32:39 +0700

    add Layout

---
+++ app/src/main/java/com/example/sijuankegor/Form_pengajuan.java
@@ -0,0 +1,14 @@
+package com.example.sijuankegor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_pengajuan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_pengajuan);
+    }
+}
---
+++ app/src/main/java/com/example/sijuankegor/Menu_login.java
@@ -0,0 +1,14 @@
+package com.example.sijuankegor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_login);
+    }
+}
